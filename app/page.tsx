5429b7ae37f746bd802c36967c7d68804262002c
mitreb <tmitreb@gmail.com>
2024-12-31 17:17:35 +0100

    Style Home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ export default async function Home() {
   const initialData = await fetchMovies(1);
 
   return (
-    <main>
-      <h2>Explore Collection</h2>
+    <main className="flex flex-col gap-10 px-8 py-16 sm:p-16">
+      <h2 className="text-white text-3xl text-center sm:text-4xl">
+        Explore Christmas Movies
+      </h2>
       <MovieList initialData={initialData} />
     </main>
   );
